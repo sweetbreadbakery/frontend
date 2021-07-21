d717a693ffd65df9e5a0d8090a07ab80faccf4ee
Eric <me@ericcolon.com>
2021-07-21 05:07:11 -0400

    added store test to console output

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -7,12 +7,18 @@ import Alpine from 'alpinejs';
  * Init Alpine.js
  */
 window.Alpine = Alpine;
-Alpine.store('loaded', false);
+Alpine.store('avime', {
+  loaded: false,
+  complete() {
+    this.loaded = true
+  },
+});
 Alpine.start();
 
 /**
  * Init everything else...
  */
 document.addEventListener('DOMContentLoaded', () => {
-  console.info($store);
+  Alpine.store('avime').complete();
+  console.info('Alpine.js Loaded: ' + Alpine.store('avime').loaded);
 });
