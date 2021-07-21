a9d5b8aa55cf12f3bbd18cc1f286ca2845255d48
Eric <me@ericcolon.com>
2021-07-21 05:17:34 -0400

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,29 +6,9 @@ module.exports = {
       './resources/**/*.{html,svg,vue,js}',
     ],
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          'Assistant',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          '"Noto Sans"',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
