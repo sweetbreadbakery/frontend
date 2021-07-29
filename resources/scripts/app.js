b1c5c3adab07e2f7c0d474872fdd37115a7395e0
Eric <me@ericcolon.com>
2021-07-29 01:16:44 -0400

    web3 is working, updated manifest info

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,11 +1,7 @@
 /**
  * Init Web3
  */
-// const web3 = new window.Web3(window.Web3.givenProvider || 'https://ropsten.infura.io/v3/aefe6ab3ce96433ba12fa12cd1c37988');
-
-// async function getBlockNumber() {
-//   return await web3.eth.getBlockNumber();
-// }
+const web3 = new window.Web3(window.Web3.givenProvider || 'https://ropsten.infura.io/v3/aefe6ab3ce96433ba12fa12cd1c37988');
 
 /**
  * Init Alpine.js
@@ -13,7 +9,8 @@
 window.Alpine.store('avime', {
   loaded: false,
   complete() {
-    this.loaded = true
+    this.loaded = true;
+    web3.eth.getBlockNumber().then(console.info);
   },
 });
 
@@ -22,7 +19,4 @@ window.Alpine.store('avime', {
  */
 document.addEventListener('DOMContentLoaded', () => {
   window.Alpine.store('avime').complete();
-
-  console.info('Alpine.js Loaded: ' + window.Alpine.store('avime').loaded);
-  // console.info(getBlockNumber());
 });
