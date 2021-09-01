0249242661e6a7fcd4320f83e6a2754efb0e59c3
Eric <me@ericcolon.com>
2021-09-01 11:05:32 -0400

    added on-chain section

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,137 @@ module.exports = {
         heading: '"cp_fontregular", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
         sans: '"Pixelated MS Sans Serif", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: [
+            {
+              color: theme('colors.black'),
+              '[class~="lead"]': {
+                color: theme('colors.black'),
+              },
+              a: {
+                color: theme('colors.blue.500'),
+              },
+              strong: {
+                color: theme('colors.black'),
+              },
+              'ol > li::before': {
+                color: theme('colors.gray.400'),
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.gray.300'),
+              },
+              hr: {
+                borderColor: theme('colors.gray.100'),
+              },
+              blockquote: {
+                color: theme('colors.black'),
+                borderLeftColor: theme('colors.gray.100'),
+              },
+              h1: {
+                fontFamily: theme('fontFamily.heading'),
+                color: theme('colors.yellow.400'),
+              },
+              h2: {
+                fontFamily: theme('fontFamily.heading'),
+                color: theme('colors.yellow.400'),
+              },
+              h3: {
+                fontFamily: theme('fontFamily.heading'),
+                color: theme('colors.blue.500'),
+              },
+              h4: {
+                fontFamily: theme('fontFamily.heading'),
+                color: theme('colors.blue.500'),
+              },
+              'figure figcaption': {
+                color: theme('colors.black'),
+              },
+              code: {
+                color: theme('colors.black'),
+              },
+              'a code': {
+                color: theme('colors.blue.500'),
+              },
+              pre: {
+                color: theme('colors.gray.100'),
+                backgroundColor: theme('colors.gray.800'),
+              },
+              thead: {
+                color: theme('colors.black'),
+                borderBottomColor: theme('colors.gray.200'),
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.gray.100'),
+              },
+            },
+          ],
+        },
+        white: {
+          css: [
+            {
+              color: theme('colors.white'),
+              '[class~="lead"]': {
+                color: theme('colors.white'),
+              },
+              a: {
+                color: theme('colors.white'),
+              },
+              strong: {
+                color: theme('colors.white'),
+              },
+              'ol > li::before': {
+                color: theme('colors.white'),
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.white'),
+              },
+              hr: {
+                borderColor: theme('colors.white'),
+              },
+              blockquote: {
+                color: theme('colors.white'),
+                borderLeftColor: theme('colors.white'),
+              },
+              h1: {
+                fontFamily: theme('fontFamily.heading'),
+                color: theme('colors.white'),
+              },
+              h2: {
+                fontFamily: theme('fontFamily.heading'),
+                color: theme('colors.white'),
+              },
+              h3: {
+                fontFamily: theme('fontFamily.heading'),
+                color: theme('colors.white'),
+              },
+              h4: {
+                fontFamily: theme('fontFamily.heading'),
+                color: theme('colors.white'),
+              },
+              'figure figcaption': {
+                color: theme('colors.white'),
+              },
+              code: {
+                color: theme('colors.white'),
+              },
+              'a code': {
+                color: theme('colors.white'),
+              },
+              pre: {
+                color: theme('colors.white'),
+              },
+              thead: {
+                color: theme('colors.white'),
+                borderBottomColor: theme('colors.white'),
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.white'),
+              },
+            },
+          ],
+        },
+      }),
       screens: {
         'xs': '375px',
         ...defaultTheme.screens,
