eccbca6f640f248bb330b01151224e9e1007e401
Eric <me@ericcolon.com>
2021-09-03 10:21:45 -0400

    added default slots

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,7 +1,6 @@
 import Alpine from 'alpinejs';
 import { faqs } from './util/faqs';
 import { staff } from './util/staff';
-import { getRandomIntInclusive } from './util/getRandomIntInclusive';
 
 /**
  * Set Alpine.js store data
@@ -19,23 +18,6 @@ Alpine.store('myAvime', {
     clothes: 'default',
     hair: 'default',
     accessory: 'default',
-  },
-  clear() {
-    this.traits.background = 'default';
-    this.traits.body = 'default';
-    this.traits.face = 'default';
-    this.traits.clothes = 'default';
-    this.traits.hair = 'default';
-    this.traits.accessory = 'default';
-  },
-  randomize(gender) {
-    this.traits.background = getRandomIntInclusive(1, 12);
-    this.traits.body = getRandomIntInclusive(1, 12);
-    this.traits.face = getRandomIntInclusive(1, 12);
-    this.traits.clothes = getRandomIntInclusive(1, 12);
-    this.traits.hair = getRandomIntInclusive(1, 12);
-    this.traits.accessory = getRandomIntInclusive(1, 12);
-    this.gender = (gender === 'female') ? 'male' : 'female';
   },
 });
 
